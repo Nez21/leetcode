94a0cfd513c99c0f39cfa82e54b7ad2fa620e146
Chuong Vo Bao Van <56601052+Nez21@users.noreply.github.com>
2024-11-08 06:36:53 -0800

    Time: 0 ms (100%), Space: 3.4 MB (44.19%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.rs
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.rs
@@ -1,28 +1,17 @@
 impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
-        let n = prices.len();
-        let mut dp = vec![vec![i32::MIN; n]; 4];
+        let mut h1 = i32::MIN;
+        let mut h2 = i32::MIN;
+        let mut r1 = 0;
+        let mut r2 = 0;
 
-        dp[0][0] = -prices[0];
-        dp[1][0] = 0;
-        dp[2][0] = -prices[0];
-        dp[3][0] = 0;
-
-        for i in 1..n {
-            dp[0][i] = dp[0][i - 1].max(-prices[i]);
-            dp[2][i] = dp[2][i - 1].max(-prices[i]);
+        for p in prices {
+            r2 = r2.max(h2 + p);
+            h2 = h2.max(r1 - p);
+            r1 = r1.max(h1 + p);
+            h1 = h1.max(-p);
         }
 
-        for i in 1..4 {
-            for j in 1..n {
-                if i % 2 == 0 {
-                    dp[i][j] = dp[i][j].max(dp[i][j - 1]).max(dp[i - 1][j - 1] - prices[j]);
-                } else {
-                    dp[i][j] = dp[i][j].max(dp[i][j - 1]).max(dp[i - 1][j - 1] + prices[j]);
-                }
-            }
-        }
-
-        dp[3][n - 1].max(0)
+        r2
     }
 }
