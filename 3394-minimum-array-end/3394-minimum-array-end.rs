7d5c6f9b53dd21dd810a392da3429a094815378e
Chuong Vo Bao Van <56601052+Nez21@users.noreply.github.com>
2024-11-09 07:01:35 -0800

    Time: 0 ms (100%), Space: 2.1 MB (100%) - LeetHub

--- 3394-minimum-array-end/3394-minimum-array-end.rs
+++ 3394-minimum-array-end/3394-minimum-array-end.rs
@@ -1,25 +1,16 @@
 impl Solution {
     pub fn min_end(mut n: i32, x: i32) -> i64 {
-        let max = (n as f32).log2().ceil() as usize;
-        let mut zeros = Vec::with_capacity(max);
-
-        for i in 0..64 {
-            if ((1u64 << i) & (x as u64)) == 0 {
-                zeros.push(i);
-            }
-
-            if zeros.len() == max {
-                break;
-            }
-        }
-
-        let mut shift = (n - 1) as u64;
         let mut res = x as u64;
+        let mut rem = (n - 1) as u64;
+        let mut pos = 1;
 
-        for i in 0..max {
-            if ((1u64 << i) & shift) != 0 {
-                res += 1u64 << zeros[i];
+        while rem > 0 {
+            if res & pos == 0 {
+                res |= (rem & 1) * pos;
+                rem >>= 1;
             }
+
+            pos <<= 1;
         }
 
         res as i64
